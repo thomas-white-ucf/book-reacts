e8a49559c7e81479fe3e7f7c04eb4be68c1a44c3
Thomas White <thomas.white.ucf@gmail.com>
2019-10-29 16:39:51 -0400

    fixed Database connection

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const mongoose = require('mongoose');
 const app = express();
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password123@ds339968.mlab.com:39968/heroku_txwt1dj1"
-mongoose.connect(MONGODB_URI);
-
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password123@ds339968.mlab.com:39968/heroku_txwt1dj1"
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Serve up static assets (usually on heroku)
 
 // Send every request to the React app
 // Define any API routes before this runs
