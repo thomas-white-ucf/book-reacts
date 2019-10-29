9d8135fb5c6ca041246131bc728d46c9f5b1c33e
Thomas White <thomas.white.ucf@gmail.com>
2019-10-29 15:37:14 -0400

    connecting to Database on herkou when deployed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import mongoose from 'mongoose';
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password123@ds339968.mlab.com:39968/heroku_txwt1dj1"
+mongoose.connect(MONGODB_URI);
+//  process.env.MONGODB_URI  || "mongodb://localhost/mongoHeadlines"
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
