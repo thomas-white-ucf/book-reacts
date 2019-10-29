4be35e227b17df3662b91faaaf9bd281dbc1aea9
Thomas White <thomas.white.ucf@gmail.com>
2019-10-29 19:56:23 -0400

    console log added

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password123@ds339968.mlab.com:39968/heroku_txwt1dj1"
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-	useUnifiedTopology: true
-});
+  useUnifiedTopology: true
+}).then(console.log(`Mongoose Database connection: ${MONGODB_URI}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Serve up static assets (usually on heroku)
 
